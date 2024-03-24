337466cd2a4495f25a9e9d66d3eb819f4b2d5766
JosoJmf <joso.jmf@gmail.com>
2024-03-24 22:24:16 +0100

    Corregido a postriori, tras entrega v2

--- islands/WordCSR.tsx
+++ islands/WordCSR.tsx
@@ -1,21 +1,12 @@
 import { FunctionComponent } from "https://esm.sh/v128/preact@10.19.6/src/index.js";
 import { Word } from "../types.ts";
 
-
-
-const WordIsland :FunctionComponent<{word:Word | undefined}>=(props)=>{
-    console.log(props.word!.word)
-    return(
-        <div className="WordSSR">
-        <div className="WordSSRTitle">
-         {props.word!.word}
-         </div>
-         <div className="Defs">
-            {props.word!.meanings}
-         </div>
-        </div>
-       
-    )
-
-}
+const WordIsland: FunctionComponent<{ word: Word | undefined }> = (props) => {
+  return (
+    <div className="WordSSR">
+      <div className="WordSSRTitle">{props.word!.word}</div>
+      <div className="Defs">{props.word!.meanings}</div>
+    </div>
+  );
+};
 export default WordIsland;
